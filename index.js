441915fe886a70c191f04e5c79155efcf1452768
Mikey Mann <mikey.mannx3@gmail.com>
2019-11-29 15:39:59 -0800

    added userlistItem varaible to store entered item and appending method to add item template to ul shopping list

---
+++ index.js
@@ -0,0 +1,31 @@
+$(function() {
+
+//First I need to remove the default submit button behavior
+$('#js-shopping-list-form').submit(event => {
+    event.preventDefault();
+
+//Now I need to create a new <li> inside the shopping list <ul class="shopping-list"
+//when there is an item inside the input box
+
+const userListItem = $(this).find('#shopping-list-entry').val();
+
+$('.shopping-list').append(
+    `<li>
+      <span class="shopping-item">${userListItem}</span>
+      <div class="shopping-item-controls">
+        <button class="shopping-item-toggle">
+          <span class="button-label">check</span>
+        </button>
+        <button class="shopping-item-delete">
+          <span class="button-label">delete</span>
+        </button>
+      </div>
+    </li>`);
+
+//clear input to add another list item
+
+$('.js-shopping-list-entry').val('');
+
+
+    }
+}
